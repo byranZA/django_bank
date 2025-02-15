908c6c429941134edc4315ec7ff7f46546f03caf
Byran <byran.taljaard@gmail.com>
2025-02-15 16:28:57 +0100

    chore: setup settings

---
+++ config/settings/local.py
@@ -0,0 +1,10 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-((d%h6k&z^wh^029$2y4u@argogjlg$a0+^90e&bt^vaimvrs1'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
